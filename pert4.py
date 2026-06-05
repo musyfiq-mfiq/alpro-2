af2644fedb9cc5e83712a168ebccbdd80911c312
MUSYFIQ FAKHRUZZAMAN <musyfiq.fakhruz@gmail.com>
2026-06-05 11:52:40 +0700

    Add basic input/output operations in pert4.py

---
+++ pert4.py
@@ -0,0 +1,67 @@
+# 1
+Es = input("paling enak:")
+print(Es, "enak banget weyy")
+
+# 2
+Angka = input("berapa usiamu :")
+print("usiaku :", Angka)
+
+# 3
+Angka = input("masukan nilai angka:")
+Hasil = Angka * 5
+print("Hasil :", Hasil)
+
+# 4
+Angka = float(input("masukan nilai angka:"))
+Hasil = Angka * 5
+print("Hasil :", Hasil)
+
+# 5
+Angka = int(input("masukan nilai angka:"))
+Hasil = Angka * 5
+print("Hasil :", Hasil)
+
+# 6
+print("+" + 10 * "-" + "+")
+print(("|" + 10 * " " + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+
+# 7
+print("Masukan nama depan anda :")
+Nama_depan = input()
+print("Masukan nama belakang anda :")
+Nama_belakang = input()
+print("Nama lengkap anda adalah: " + Nama_depan + " " + Nama_belakang)
+
+# 8
+print("Halo " + input("siapa namamu :") + ", salam kenal")
+
+# 9
+print("Perkalian")
+Angka1 = float(input("Masukan angka ke-1 :"))
+Angka2 = float(input("Masukan angka ke-2 :"))
+Hasil = Angka1 * Angka2
+print("Hasil perkalian kedua angka adalah:", Hasil)
+
+# 10
+print("Pembagian")
+Angka1 = float(input("Masukan angka ke-1 :"))
+Angka2 = float(input("Masukan angka ke-2 :"))
+Hasil = Angka1 / Angka2
+print("Hasil pembagian kedua angka adalah:", Hasil)
+
+# 11
+Kata1 = input("Masukan kata ke-1 :")
+Kata2 = input("Masukan kata ke-2 :")
+print("Hasil penggabungan kata adalah :", Kata1 + Kata2)
+
+# 12
+x = float(input("Masukan nilai x :"))
+y = 1 / (x + 1 / (x + 1 / (x + 1 / x)))
+print("Hasil y =", y)
+
+# 13
+Jam = int(input("Masukan jam mulai :"))
+Menit = int(input("Masukan menit mulai :"))
+Durasi = int(input("Masukan durasi (menit) :"))
+Total_men
