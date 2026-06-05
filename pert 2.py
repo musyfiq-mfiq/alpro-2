6bb8e3e646f5fefe1fc5ad40196999cb19c6031f
MUSYFIQ FAKHRUZZAMAN <musyfiq.fakhruz@gmail.com>
2026-06-05 11:33:32 +0700

    Add initial Python script with various print statements

---
+++ pert 2.py
@@ -0,0 +1,92 @@
+```python
+# 1
+nama = "taohid" 
+umur = "19" 
+th = "tahun" 
+satuan = "cm" 
+tinggi = "180" 
+
+print ("nama:", nama) 
+print ("umur:", umur, th) 
+print ("tinggi:", tinggi, satuan) 
+
+# 2
+angkaPositif = 11111111 
+angkaNegatif = -11111111 
+print("Angka Positif :", angkaPositif) 
+print("Angka Negatif :", angkaNegatif) 
+
+# 3
+print (0o25) 
+print (0x19) 
+
+# 4
+print(0.4)
+print(4.0)
+print(.4)
+print(4.)
+print(1.5e1) 
+print(2.5e-1) 
+print(3.0e+2) 
+
+# 5
+print ("selamat pagi dunia, hai... ") 
+print ('selamat siang dunia, hai...') 
+
+# 6
+print (True) 
+print (False) 
+
+# 7
+print (4**7) 
+print (4**7.5) 
+print (4.5**7) 
+print (4.5**7.5) 
+
+# 8
+print (2*6) 
+print (2*6.0) 
+print (2.0*6) 
+print( 2.0*6.0) 
+
+# 9
+print(10/2)
+print(10/2.5)
+print(10.5/2)
+print(10.5/2.5)
+
+# 10
+print(10//2) 
+print(10//2.5) 
+print(10.5//2) 
+print(10.5//2.5) 
+
+# 11
+print(10% 3) 
+print(20% 3.5) 
+print(30.5% 3) 
+print(40.5% 3.5) 
+
+# 12
+print(-2) 
+print(+3) 
+print(not True) 
+print(not False) 
+print(5+5) 
+print(5-3) 
+print(8*3) 
+print (24/3) 
+print (10//3) 
+print(72%3) 
+print(4**3) 
+
+# 13
+print (5*(5+5)) 
+print( (30-6)/2) 
+
+# 14
+print ((5**5),(2*5),(5*2))
+print((-2/4),(2/4),(2//4),(-2//4))
+print((2%-4),(2%4),(2**3**2))
+
+```
